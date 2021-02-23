3e11263b2e93ad1111a75fdf89c4b64d7ec90374
Tom Goldsmith <tom@7theta.com>
2021-02-22 21:10:30 -0500

    Update deps.

--- project.clj
+++ project.clj
@@ -8,7 +8,7 @@
 ;;   the terms of this license.
 ;;   You must not remove this notice, or any others, from this software.
 
-(defproject com.7theta/cella-rn "0.3.3"
+(defproject com.7theta/cella-rn "0.3.4"
   :description "A library to wrap React Native Async Storage"
   :url "https://github.com7theta/cella-rn"
   :license {:name "Eclipse Public License"
@@ -17,9 +17,9 @@
                  [inflections "0.13.2"]
                  [integrant "0.8.0"]
                  [com.7theta/utilis "1.12.1"]
-                 [com.7theta/tempus "0.2.0"]
+                 [com.7theta/tempus "0.3.0"]
                  [metosin/malli "0.2.1"]
-                 [borkdude/sci "0.2.1"]]
+                 [borkdude/sci "0.2.3"]]
   :profiles {:dev {:dependencies [[org.clojure/clojure "1.10.2"]
                                   [org.clojure/clojurescript "1.10.773"]
                                   [integrant/repl "0.3.2"]]}}
